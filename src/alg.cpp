31b04b330e618c7d2942d8c8bcaf41f43a03ae5f
ngtolstova <124882480+ngtolstova@users.noreply.github.com>
2023-02-17 19:42:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,17 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+    int del;
+    if(a=b){
+        cout << a << endl;
+        return 1;
+    }
+    if(a>b) 
+        del = b;
+    else 
+        del = a;
+    while(a%del==0 && b%del==0)
+        del--;
+    return del;
 }
+
